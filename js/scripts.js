14c3f1927c9bb9f0a0f4c8c5420da59a55e4501e
mhoblin <monicahoblin@eagles.ewu.edu>
2015-05-05 13:21:29 -0700

    text

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,13 @@ $(document).ready(function(){
   
 var msg0 = 'Too great of an ultimatum. Take no action or try again.';
 var msg1 = 'I think you are missing some things. Add more blocks and retry.';
-var msg2 = 'Hola!';
-var msg3 = 'Hello, kitten';
+var msg2 = 'something 3';
+var msg3 = 'something 4';
+var msg4 = 'something 5';
+var msg5 = 'something 6';
+var msg6 = 'something 7';
+var msg7 = 'something 8';
+var msg8 = 'something 9';
 
 console.log( msg0, msg1, msg2, msg3 );
 
