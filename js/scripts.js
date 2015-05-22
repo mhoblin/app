f9f233c0b708e94f963e9bba6a0889a3c3cd08ae
mhoblin <monicahoblin@eagles.ewu.edu>
2015-05-21 20:23:01 -0700

    good for now

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,17 @@ var msg2 = 'One of your blocks is on the wrong side. Move it and try again.';
 var msg3 = 'Exchange the largest block on the left with the smallest block on the right and try again.';
 var msg4 = 'You are on suicidal watch. Add another block to "reasons to leave" and try again.';
 var msg5 = 'Your abuser apologized and agrees to go to counceling. Add a block to reasons to stay.';
-var msg6 = 'something 7';
-var msg7 = 'something 8';
-var msg8 = 'something 9';
-var msg9 = 'something 9';
-var msg10 = 'something 9';
-var msg11 = 'something 9';
-var msg12 = 'something 9';
-var msg13 = 'something 9';
-var msg14 = 'something 9';
+var msg6 = 'Exchange the largest block on the right with the smallest block on the left and try again.';
+var msg7 = 'Your abuser bought a gun. If it is not there already, add "I will be killed" as a reason to leave';
 
-console.log( msg0, msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8, msg9, msg10, msg11, msg12, msg13, msg14 );
+
+console.log( msg0, msg1, msg2, msg3, msg4, msg5, msg6, msg7 );
 
 
 
 // 2. Get random number
 // returns 0 to 'limit'
-var limit = 15;
+var limit = 8;
 var numRand = Math.floor(Math.random()*limit);
 
 // 3. if random number 0, show first message, etc. 
